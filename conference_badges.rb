057aca883d8ec5d4d4c40145e7047af6904db3fe
Ken Barlow <kenbrlow@gmail.com>
2020-08-27 20:36:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,8 @@
 # Write your code here.
 def badge_maker(name)
-  "Hello, my name is #{name}."
+  message = "Hello, my name is #{name}."
+  printer(message)
+  message
 end
 
 def batch_badge_creator(array)
