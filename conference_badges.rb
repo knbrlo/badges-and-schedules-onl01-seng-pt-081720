f4e8c3ad21c4878cc2267ad10c25e422df591cc2
Ken Barlow <kenbrlow@gmail.com>
2020-08-27 20:18:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end
+
+def assign_rooms(array)
+  
+end
+
+def batch_badge_creator(array)
+  array_messages = []
+  array.each do |name|
+    array_messages << badge_maker(name)
+  end
+  array_messages
+end
+
+def printer(array)
+
+end
